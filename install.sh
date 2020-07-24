f5e5e51e2004056c09edb3d255587f6625e91923
Tuhin Kanti Pal <51857187+cachecleanerjeet@users.noreply.github.com>
2020-07-24 15:51:56 +0530

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-sudo -i
-git clone https://github.com/cachecleanerjeet/adda.git
-cd adda
-npm install
-npm install forever -g
-forever start src/app.js
-echo 'Adda is Running on Port 8080'
-echo 'Visit https://tu.hin.life'
